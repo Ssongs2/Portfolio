9cbe44f935985479414ebba1b2d91c89215f35f2
Ssongs2 <sejeq1995@gmail.com>
2023-03-07 17:22:52 +0900

    aboutMe, skill css

--- src/components/pages/Skill.js
+++ src/components/pages/Skill.js
@@ -5,22 +5,21 @@ import apple from '../../assets/resources/apple.png'
 function Skill() {
     return (
         <section className='section skill'>
-            <div>
-                <div style={{ "position": "absolute", "top": "24%" }}>
-                    <img width="200px" height="200px" src={apple} alt=''></img>
-                </div>
-                <div style={{ "position": "absolute", "top": "30%", "left": "50%" }}>
-                    <img width="200px" height="200px" src={apple} alt=''></img>
-                </div>
-                <div style={{ "position": "absolute", "top": "50%", "right": "0%" }}>
-                    <img width="200px" height="200px" src={apple} alt=''></img>
-                </div>
-                <div style={{ "position": "absolute", "top": "20%", "left": "35%" }}>
-                    <img width="200px" height="200px" src={apple} alt=''></img>
-                </div>
-                <div style={{ "textAlign": "center", "position": "absolute", "top": "50%", "left": "50%", "transform": "translate(-50%, -30%)" }}>
-                    <img width="600px" height="500px" src={bowl} alt=''></img>
-                </div>
+            <div className="imgWrapper" id="a1" style={{ "width": "100%" }}>
+                <img className="apple" style={{ "maxWidth": "100%", "height": "auto" }} src={apple} alt=''></img>
+            </div>
+            <div className="imgWrapper" id="a2" style={{ "width": "100%" }}>
+                <img className="apple" style={{ "maxWidth": "100%", "height": "auto" }} src={apple} alt=''></img>
+            </div>
+            <div className="imgWrapper" id="a3" style={{ "width": "100%" }}>
+                <img className="apple" style={{ "maxWidth": "100%", "height": "auto" }} src={apple} alt=''></img>
+            </div>
+            <div className="imgWrapper" id="a4" style={{ "width": "100%" }}>
+                <img className="apple" style={{ "maxWidth": "100%", "height": "auto" }} src={apple} alt=''></img>
+            </div>
+            <div className="imgWrapper bowl">
+                <img className="bowl" style={{ "maxWidth": "100%", "height": "auto" }} src={bowl} alt=''></img>
+
             </div>
         </section>
     )
